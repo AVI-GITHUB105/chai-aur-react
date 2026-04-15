fc224ca1e7d6871a09d975e4b3ee016b3574b105
A-aniket-k <aniketk6664@gmail.com>
2026-04-15 12:11:28 +0530

    code edit to run

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./Chai"
 function App() {
-
+  const username = "User"; // Define the username variable
 
   return (
     // <h1>chai aur vite</h1> 
     // hum ek hi elemnt export kr sakte h h1 wala bhi likhenge to error ayega so uske liye div ya fragment(<> </>) use krlo 
     // <Chai/>
-     <>
+    <>
     <Chai/>
     <h1>chai aur react {username}</h1>
     <p>test para</p>
