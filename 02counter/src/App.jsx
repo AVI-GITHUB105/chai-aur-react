fd7cf765b274b80f86163296569975c569c17cc1
A-aniket-k <aniketk6664@gmail.com>
2026-04-15 18:13:20 +0530

    counter if condition

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,15 +9,22 @@ let [counter,setcounter] = useState(15)// can write anything 0 ... instead of 15
 
 //  let counter = 15 just variable change hoga ui nhi
 const incvalue = function (){
-
-  counter = counter + 1; //isse value inc to ho rhi h inconsole but
+  if (counter == 20){
+    alert('counter cant be greater then 20')
+  }
+  else{counter = counter + 1; //isse value inc to ho rhi h inconsole but
 //  page pr changes nhi a rhe ui updation
 setcounter(counter);
-  console.log("value inc", counter )
+  console.log("value inc", counter )}
+  
 }
 const decvalue = function (){
-setcounter(counter - 1);
-  console.log("value dec", counter )
+  if (counter == 0){
+    alert('counter cant be lesss then zero')
+  }
+  else{setcounter(counter - 1);
+  console.log("value dec", counter )}
+
 }
   return (
     <>
