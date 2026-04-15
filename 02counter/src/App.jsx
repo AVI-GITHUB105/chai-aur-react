ba5eda7a00d2a72f770cea69b1898470ac7ac52c
A-aniket-k <aniketk6664@gmail.com>
2026-04-15 18:06:31 +0530

    change in propogation useing hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+let [counter,setcounter] = useState(15)// can write anything 0 ... instead of 15
+
+//  let counter = 15 just variable change hoga ui nhi
+const incvalue = function (){
+
+  counter = counter + 1; //isse value inc to ho rhi h inconsole but
+//  page pr changes nhi a rhe ui updation
+setcounter(counter);
+  console.log("value inc", counter )
+}
+const decvalue = function (){
+setcounter(counter - 1);
+  console.log("value dec", counter )
+}
+  return (
+    <>
+    <h1> Chai counter Project</h1>
+    <h2>counter value: {counter}</h2> 
+
+    <button onClick={incvalue} >inc value {counter}</button>
+    <button onClick={decvalue}>dec value {counter} </button>
+    </>
+  )
+}
+
+export default App
